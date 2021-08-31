b1f9e6b7acc682a2b8a8f14df4d63102a5349ed3
Jonathon <bloomfield.codes@gmail.com>
2021-08-31 15:11:30 +0000

    Added login and logout function with cookies

--- express_server.js
+++ express_server.js
@@ -5,6 +5,8 @@ const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
+var cookieParser = require('cookie-parser')
+app.use(cookieParser())
 
 app.post("/urls", (req, res) => {
   // console.log(req.body);  // Log the POST request body to the console
@@ -36,16 +38,23 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { 
+    urls: urlDatabase, 
+    username: req.cookies["username"],
+   };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {username: req.cookies["username"]}
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  const templateVars = { 
+    shortURL: req.params.shortURL, 
+    longURL: urlDatabase[req.params.shortURL],
+    username: req.cookies["username"], };
   res.render("urls_show", templateVars);
 });
  
@@ -67,6 +76,16 @@ app.post("/urls/:shortURL/edit", (req, res) => {
 app.get("/urls/:shortURL/edit", (req, res) => {
   console.log(req.params);
   res.redirect("/urls/:shortURL");
+});
+
+app.post("/login", (req, res) => {
+  res.cookie("username",req.body.username)
+  res.redirect("/urls");
+});
+
+app.post("/logout", (req, res) => {
+  res.clearCookie("username")
+  res.redirect("/urls");
 });
 
 app.listen(PORT, () => {
